c4f18160703996d1a4ed55fde38d233be22c5b27
Xelofan <39835534+Xelofan@users.noreply.github.com>
2024-02-13 09:18:33 +0100

    Update ansible-script.sh

--- ansible-script.sh
+++ ansible-script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 ansible-pull -U https://github.com/xelofan/ansible install-dotfiles.yml -K
+
+## > Git
+## - Email: git config --global user.email "EMAIL"
+## - Username: git config --global user.name "NÉV"
+
