01b340005b0989cb06fdb7d4c262d851b1d57d8e
Xelofan <39835534+Xelofan@users.noreply.github.com>
2025-04-12 19:47:00 +0200

    Added automations + extras

--- ansible-script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ansible-pull -U https://github.com/xelofan/ansible install-dotfiles.yml -K
---
+++ script.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+GRN="\033[0;32m"
+RED="\033[0;31m"
+YLW="\033[0;33m"
+NC="\033[0m"
+
+msg(){
+  echo -e "\n[DOTFILES: ${YLW}Info${NC}] $1\n"
+}
+suc(){
+  echo -e "\n[DOTFILES: ${GRN}Success${NC}] $1\n"
+}
+err(){
+  echo -e "\n[DOTFILES: ${RED}Error${NC}] $1\n"
+}
+
+# Function to check if command exists
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+cmd() {
+  if [ "$(id -u)" -eq 0 ]; then
+    $@
+  else
+    ASKPASS="-K"
+    sudo $@
+  fi
+}
+
+# Detect the operating system
+if [ -f /etc/os-release ]; then
+  # For Linux distributions with /etc/os-release file
+  source /etc/os-release
+  OS=$ID
+  VERSION_ID=$VERSION_ID
+elif [ "$(uname)" = "Darwin" ]; then
+  # For macOS
+  OS="darwin"
+  VERSION_ID=$(sw_vers -productVersion)
+  NAME=$(sw_vers -productName)
+else
+  err "Undetectable operating system, exiting.."
+  exit 1
+fi
+
+msg "Detected OS: $NAME ($VERSION_ID), installing ansible, git.."
+
+case $OS in
+  ubuntu)
+    if [ $VERSION_ID = "22.04" ]; then
+      cmd apt-get update
+      cmd apt-get install curl git -y
+      
+      if ! command_exists pip3; then
+        cmd apt-get install python3-pip -y
+      fi
+      
+      if [ "$(id -u)" -eq 0 ]; then
+        pip3 install ansible
+      else
+        pip3 install --user ansible
+      fi
+      
+    elif [ "$(echo $VERSION_ID | cut -d '.' -f 1)" -ge 24 ]; then
+      cmd apt-get update
+      cmd apt-get install curl git ansible -y
+    fi
+    ;;
+    
+  debian)
+    if [ $VERSION_ID -ge 12 ]; then
+      cmd apt-get update
+      cmd apt-get install curl git ansible -y
+    fi
+    ;;
+    
+  opensuse*)
+    cmd zypper refresh
+    
+    # Install curl, git, and ansible
+    cmd zypper -n install curl git ansible
+    ;;
+    
+  arch)
+    cmd pacman -Sy
+    cmd pacman -S --noconfirm curl git ansible
+    ;;
+    
+  darwin)
+    # if ! command_exists brew; then
+    #   err "Homebrew not found, installing..."
+    #   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # fi
+    brew install curl git ansible
+    ;;
+    
+  *)
+    err "Unsupported operating system found: $NAME ($VERSION_ID)"
+    exit 1
+    ;;
+esac
+
+suc "Installation completed"
+
+msg "Running dotfiles playbook.."
+
+if command_exists ansible-pull; then
+  ansible-pull -U https://github.com/xelofan/dotfiles playbook.yml $ASKPASS
+else
+  err "Could not run 'ansible-pull', exiting.."
+  exit 1
+fi
